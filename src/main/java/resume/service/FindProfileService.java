8620289b07d29106221efa22d476e27a8b2965af
Ice <Ice@DESKTOP-S3I6KOA>
2019-10-04 02:56:46 +0300

    Fix project

---
+++ src/main/java/resume/service/FindProfileService.java
@@ -0,0 +1,22 @@
+package resume.service;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import resume.entity.Profile;
+
+/**
+ * 
+ * @author devstudy
+ * @see http://devstudy.net
+ */
+public interface FindProfileService {
+
+	Profile findByUid(String uid);
+	
+	Page<Profile> findAll(Pageable pageable);
+	
+	Iterable<Profile> findAllForIndexing();
+	
+	Page<Profile> findBySearchQuery(String query, Pageable pageable);
+}
